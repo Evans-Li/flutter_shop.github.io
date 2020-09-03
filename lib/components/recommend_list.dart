0279735016d0e88252403c1e21465f5401ba1483
Axing <43921905+LXxxx1@users.noreply.github.com>
2020-09-03 14:39:16 +0800

    商品推荐 完成

---
+++ lib/components/recommend_list.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class Recommend extends StatelessWidget {
+  final List recommendList;
+  Recommend({Key key, this.recommendList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(230.0),
+      // width: ScreenUtil().setWidth(750.0),
+      margin: EdgeInsets.only(top:10.0),
+      child: Column(
+        children: [
+          _titleWidget(),
+          _recommendList()
+        ],
+      ),
+    );
+  }
+
+  Widget _recommendList(){
+    return Expanded(
+      // width: ScreenUtil().setWidth(750.0),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recommendList.length,
+        itemBuilder: (context, int index){
+          return _itemWidget(index);
+        },
+      ),
+    );
+  }
+  Widget _itemWidget(index){
+    return Container(
+      width: ScreenUtil().setWidth(250.0),
+      height: ScreenUtil().setHeight(230.0),
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          left: BorderSide(color: Colors.black12, width: 1)
+        )
+      ),
+      child: InkWell(
+        onTap: (){},
+        child: Column(
+          children: [
+            Image.network(
+              recommendList[index]['img'],
+              ),
+            Text('¥${recommendList[index]["mall_price"]}'),
+            Text(
+              '¥${recommendList[index]["price"]}',
+              style: TextStyle(
+                color: Colors.black12,
+                decoration: TextDecoration.lineThrough
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 推荐商品title组件
+  Widget _titleWidget(){
+    return Container(
+      alignment: Alignment.centerLeft,
+      decoration: BoxDecoration(
+        color: Colors.green[200],
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Colors.green[200])
+        )
+      ),
+      child: Text(
+        '商品推荐',
+        style: TextStyle(color: Colors.black),
+      ),
+    ); 
+  }
+
+}
--- lib/pages/home_tab_page/home_page.dart
+++ lib/pages/home_tab_page/home_page.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_screenutil/screenutil.dart';
 import 'package:flutter_shop/components/adBanner.dart';
-import 'package:flutter_shop/components/launcher_phone.dart';
+import 'package:flutter_shop/components/recommend_list.dart';
 import 'dart:async';
-import 'dart:io';
 import '../../config/service_url.dart';
 import '../../components/banner.dart';
 
@@ -36,45 +36,41 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
-        child: FutureBuilder(
+        body: Container(
+      child: FutureBuilder(
         future: fetchDate(),
-        builder: (context, snapshot){
+        builder: (context, snapshot) {
           if (snapshot.hasData) {
             // var data = json.decode(snapshot.data.toString());
             var data = snapshot.data;
             List<Map> bannerList = (data['data']['bannerList'] as List).cast(); // banner图
             List<Map> cateList = (data['data']['cateList'] as List).cast(); // 分类
+            List<Map> recommendList = (data['data']['recommendList'] as List).cast(); // 推荐
             String _adPicture = data['data']['adPicture'][0]['ad_picture']; // 广告
-            var _bossInfo = data['data']['company'];  //店长信息
-            String _bossPhone = _bossInfo[0]['phone']; // 店长电话
-            String _bossImg = _bossInfo[0]['boss_img'];  // 店长图片
-            print(_bossInfo);
-            return Column(
-              children: [
-                BannerDiy(list: bannerList),
-                TopNavgatior(topNavList: cateList),
-                AdBanner(ad_picture: _adPicture),
-                LauncherPhone(bossImg: _bossImg, bossPhone: _bossPhone,)
-              ],
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  BannerDiy(list: bannerList),
+                  TopNavgatior(topNavList: cateList),
+                  AdBanner(ad_picture: _adPicture),
+                  Recommend(recommendList: recommendList),
+                  // LauncherPhone(bossImg: _bossImg, bossPhone: _bossPhone,)
+                ],
+              ),
             );
-          } else{
-            return Center(
-              child:
-              Text('加载中...')
-            );
+          } else {
+            return Center(child: Text('加载中...'));
           }
         },
       ),
-      )
-    );
+    ));
   }
 }
 
- // 首页分类导航
- class TopNavgatior extends StatelessWidget{
+// 首页分类导航
+class TopNavgatior extends StatelessWidget {
   final List topNavList;
-  TopNavgatior({Key key,this.topNavList}): super(key:key);
+  TopNavgatior({Key key, this.topNavList}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -82,34 +78,31 @@ class _HomePageState extends State<HomePage> {
       height: ScreenUtil().setHeight(260),
       padding: EdgeInsets.all(4),
       child: GridView.count(
-        crossAxisCount: 5,
-        padding: EdgeInsets.all(5),
-        children: topNavList.map((e){
-          return _gridViewItemUI(context, e);
-        }).toList()
-      ) ,
+          crossAxisCount: 5,
+          padding: EdgeInsets.all(5),
+          children: topNavList.map((e) {
+            return _gridViewItemUI(context, e);
+          }).toList()),
     );
   }
 
   @override
-  Widget _gridViewItemUI (BuildContext context, item) {
+  Widget _gridViewItemUI(BuildContext context, item) {
     return InkWell(
-      onTap: (){
-        print('点击了导航');
-      },
-      child: Container(
-        // padding: EdgeInsets.all(4),
-        child: Column(
-          children: [
-            Image.network(item['img'],width: ScreenUtil().setWidth(95),),
-            Text(item['name']),
-          ],
-      ),
-      )
-      
-    );
+        onTap: () {
+          print('点击了导航');
+        },
+        child: Container(
+          // padding: EdgeInsets.all(4),
+          child: Column(
+            children: [
+              Image.network(
+                item['img'],
+                width: ScreenUtil().setWidth(95),
+              ),
+              Text(item['name']),
+            ],
+          ),
+        ));
   }
 }
-
-
-
