a33405c93955da4edd4ea2d224818b3856655230
Axing <43921905+LXxxx1@users.noreply.github.com>
2020-09-13 18:04:25 +0800

    :bug: fixbug

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,7 +1,8 @@
-const serviceUrl = 'http://172.24.88.183:7001/default/';
+const serviceUrl = 'http://172.24.88.135:7001/default/';
 // const serviceUrl = 'http://127.0.0.1:7001/default/';
 
 const servicePath = {
   'homeData': serviceUrl+'homeData',// 首页数据
   'homeHotGoods': serviceUrl + 'getHomeHotGoods',  // 火爆商品 首页上拉加载商品
+  'categoty': serviceUrl + 'getCategory', // 分类页
 };
